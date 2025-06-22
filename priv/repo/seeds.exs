131310403e87e0580cac0fafddea2ecd672a0511
Fly Dev <dev@fly.io>
2025-06-22 21:49:02 +0000

    add test user for login

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,12 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+# Create a test user for development
+{:ok, user} =
+  Proofie.Accounts.register_user(%{email: "admin@example.com", password: "password123456"})
+
+{:ok, _user, _expired_tokens} =
+  Proofie.Accounts.update_user_password(user, %{password: "password123456"})
+
+IO.puts("Created test user: admin@example.com / password123456")
